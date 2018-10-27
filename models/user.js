c233767aff15160cae2d35cf0d15a300e066cf85
theGrandmaster05 <israelakintunde005@gmail.com>
2018-10-27 06:42:16 +0100

    Exported module

---
+++ models/user.js
@@ -0,0 +1,1 @@
+let mongoose = require('mongoose');let Schema = mongoose.Schema;let userSchema = new Schema({    email: {        type: String,        required: true    },    password: {        type: String,        required: true    }});module.exports = mongoose.model('user', userSchema);
