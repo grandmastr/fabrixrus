c71f63be760b4731dc589fbdef76a96c433b1ff8
theGrandmaster05 <israelakintunde005@gmail.com>
2018-10-28 20:04:41 +0100

    Changes

--- config/passport.js
+++ config/passport.js
@@ -1,1 +1,1 @@
-let passport = require('passport');let User = require('../models/user');let localStorage = require('passport-local');
+let passport = require('passport');let User = require('../models/user');let localStorage = require('passport-local').Strategy;passport.serializeUser((user,done) => {    done(null, user.id);});passport.deserializeUser((id,done) => {    User.findById(id, (err,user) => {        done(err, user);    });});
