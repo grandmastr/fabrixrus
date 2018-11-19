8bb365185d04d87011e1b67d774a274f5fc169cd
theGrandmaster05 <israelakintunde005@gmail.com>
2018-11-19 17:31:57 +0100

    Changes

--- helpers/auth.js
+++ helpers/auth.js
@@ -5,5 +5,12 @@ module.exports = {
         } else {
             res.redirect(302, '/user/login');
         }
+    },
+    ensureUserIsAdmin: (req,res,next) => {
+        if(req.isAuthenticated() && req.user.isAdmin === 'admin') {
+            return next()
+        } else {
+            res.redirect(302,'/admin/login');
+        }
     }
 }
