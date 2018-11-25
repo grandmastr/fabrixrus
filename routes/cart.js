ab73be1ffb490065539794471968616498707297
theGrandmaster05 <israelakintunde005@gmail.com>
2018-11-25 22:55:13 +0100

    Changes

--- routes/cart.js
+++ routes/cart.js
@@ -2,22 +2,48 @@ const express = require('express')
     , router = express.Router()
     , passport = require('passport')
     , LocalStrategy = require('passport-local').Strategy;
+const Product = require('../models/product');
 
 router.get('/', (req, res) => {
-    let cart = req.session.cart;
-    console.log(cart)
+    let cart = req.session.cart || {};
     let displayCart = {
         items: [],
         total: 0
     }
     let totalAmount = 0;
-    // for (let product of cart) {
-    //     displayCart.items.push(product);
-    //     totalAmount += (product.qty * product.price);
-    // }
+    for (let product in cart) {
+        displayCart.items.push(cart[product]);
+        totalAmount += (cart[product].price * cart[product].qty);
+    }
     displayCart.total = totalAmount;
+    console.log(cart);
+    console.log(displayCart.items);
     res.render('products/cart',{
-        // products: displayCart
+        products: displayCart
     });
 });
+
+router.post('/:id', (req,res) => {
+    req.session.cart = req.session.cart || {};
+    let cart = req.session.cart;
+    Product.findOne({
+        _id: req.params.id
+    }, (err, product) => {
+        if (err) throw err;
+        if (cart[req.params.id]) {
+            cart[req.params.id].qty++;
+        } else {
+            let { _id:id, title, price, imagePath1 } = product;
+            cart[req.params.id] = {
+                id: id,
+                title: title,
+                price: price,
+                imagePath: imagePath1,
+                qty: 1
+            }
+        }
+        res.redirect('/cart');
+    });
+});
+
 module.exports = router;
