7048c7471298395c4576e055dd225b8e55af1e6a
theGrandmaster05 <israelakintunde005@gmail.com>
2018-11-29 13:18:32 +0100

    Changes

--- public/js/main.js
+++ public/js/main.js
@@ -18,10 +18,32 @@ $(document).ready(function () {
         });
     });
     $('#cartCheckout').click(function(e) {
-        // e.preventDefault();
-        let handler = PaystackPop.setup({
-
-        });
+        e.preventDefault();
+        if ($('#userMail').val() !== '' || $('#userPhone').val() !== '') {
+            let handler = PaystackPop.setup({
+                key: 'pk_test_995e7ded5c6290da35467fc3dd442829aea4b0ee',
+                email: $('#userMail').val(),
+                amount: Number($('#totalPrice').text()) * 100,
+                metadata: {
+                    custom_fields: [
+                        {
+                            display_name: "Mobile Number",
+                            variable_name: "mobile_number",
+                            value: $('#userPhone').val()
+                        }
+                    ]
+                },
+                callback: (response) => {
+                    alert(`Transaction ref is ${response}`);
+                },
+                onClose: () => {
+                    alert('Transaction cancelled');
+                }
+            });
+            handler.openIframe();
+        } else {
+            alert('Fill the details to continue with the transaction')
+        }
     });
 });
 (function (w, d) {
