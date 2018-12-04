ac4c46548a499bf245a16c858a9e345c37528eff
theGrandmaster05 <israelakintunde005@gmail.com>
2018-12-04 08:29:27 +0100

    Changes

---
+++ models/pages.js
@@ -0,0 +1,19 @@
+let mongoose = require('mongoose');
+let Schema = mongoose.Schema;
+
+let schema = new Schema({
+    pageName :{
+        type: String,
+        required: true
+    },
+    pageContent:{
+        type: String,
+        required: true
+    },
+    dateLastUpdated: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+let Page = module.exports = mongoose.model('Page', schema);
